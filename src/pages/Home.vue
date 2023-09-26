763ec5be7a819c601b2fc2eaca3d7b028c006da5
Vladimir Orlov <86786994+happycrew@users.noreply.github.com>
2023-09-26 16:33:45 +0300

    feat: add AppLayout component (#2)

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,29 @@
-<script setup></script>
+<script setup>
+import AppLayout from '../components/AppLayout.vue';
+</script>
 
-<template>Home</template>
+<template>
+  <AppLayout imgUrl="/src/assets/img/cocktail-bg.jpg">
+    <div class="wrapper">
+      <div class="info">
+        <div class="title">Choose your drink</div>
+        <div class="line"></div>
+      </div>
+    </div>
+  </AppLayout>
+</template>
 
 <style lang="scss" scoped>
 @import '../assets/styles/main.scss';
+
+.wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.info {
+  padding: 80px 0;
+  text-align: center;
+}
 </style>
