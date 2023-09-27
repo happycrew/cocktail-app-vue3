acf029e7a959092fd4a025166a2f59696170d486
Vladimir Orlov <satancrew.true@gmail.com>
2023-09-27 16:04:35 +0300

    feat: add ingredient after click

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,20 +1,25 @@
 <script setup>
 import { ref } from 'vue';
 import AppLayout from '@/components/AppLayout.vue';
+import CocktailThumb from '../components/CocktailThumb.vue';
 import { useRootStore } from '../stores/root';
 import { storeToRefs } from 'pinia';
 
 const rootStore = useRootStore();
-const test = rootStore.getIngredients();
+rootStore.getIngredients();
 
-const { ingredients } = storeToRefs(rootStore);
+const { ingredients, cocktails } = storeToRefs(rootStore);
 const ingredient = ref(null);
+
+function getCocktail() {
+  rootStore.getCocktails(ingredient.value);
+}
 </script>
 
 <template>
   <AppLayout imgUrl="/src/assets/img/cocktail-bg.jpg">
     <div class="wrapper">
-      <div class="info">
+      <div v-if="!ingredient || !cocktails" class="info">
         <div class="title">Choose your drink</div>
         <div class="line"></div>
         <div class="select-wrapper">
@@ -22,13 +27,29 @@ const ingredient = ref(null);
             class="select"
             v-model="ingredient"
             placeholder="Choose main ingredient"
-            size="large">
+            size="large"
+            @change="getCocktail">
             <el-option
               v-for="item in ingredients"
               :key="item.strIngredient1"
               :label="item.strIngredient1"
               :value="item.strIngredient1" />
           </el-select>
+        </div>
+        <div class="text">
+          Try our delicious cocktail recipes for every occasion. Find delicious
+          cocktail recipes by ingredient through our cocktail generator.
+        </div>
+        <img src="/src/assets/img/cocktails.png" alt="Cocktails" class="img" />
+      </div>
+      <div v-else class="info">
+        <div class="title">СOCKTAILS WITH {{ ingredient }}</div>
+        <div class="line"></div>
+        <div class="cocktails">
+          <CocktailThumb
+            v-for="cocktail in cocktails"
+            :key="cocktail.idDrink"
+            :cocktail="cocktail" />
         </div>
       </div>
     </div>
@@ -55,5 +76,28 @@ const ingredient = ref(null);
 
 .select {
   width: 220px;
+}
+
+.text {
+  max-width: 516px;
+  margin: 0 auto;
+  padding-top: 50px;
+  line-height: 36px;
+  letter-spacing: 0.1em;
+  color: $second-text;
+}
+
+.img {
+  margin-top: 60px;
+}
+
+.cocktails {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  margin-top: 60px;
+  max-height: 600px;
+  overflow-y: auto;
+
 }
 </style>
--- src/stores/root.js
+++ src/stores/root.js
@@ -1,17 +1,21 @@
 import { defineStore } from 'pinia';
 import axios from 'axios';
-import { INGREDIENTS_URL } from '../constants/api';
-
+import { INGREDIENTS_URL, COCKTAILS_BY_INGREDIENT } from '../constants/api';
 
 export const useRootStore = defineStore('root', {
   state: () => ({
     ingredients: [],
+    cocktails: [],
   }),
   actions: {
     async getIngredients() {
-      console.log(URL);
       const data = await axios.get(INGREDIENTS_URL);
       this.ingredients = data?.data?.drinks;
+    },
+    async getCocktails(ingredient) {
+      const data = await axios.get(`${COCKTAILS_BY_INGREDIENT}${ingredient}`);
+      this.cocktails = data?.data?.drinks;
+      console.log(this.cocktails);
     },
   },
 });
