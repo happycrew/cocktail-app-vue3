fdfa26e1e624ff6947fc041992a92e0c7a387933
Vladimir Orlov <86786994+happycrew@users.noreply.github.com>
2023-09-27 16:16:09 +0300

    feat: add ingredient after click (#5)

---
+++ src/components/CocktailThumb.vue
@@ -0,0 +1,45 @@
+<script setup>
+const props = defineProps({
+  cocktail: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <RouterLink :to="`/cocktails/${cocktail.idDrink}`">
+    <div class="root">
+      <div
+        :style="`background-image: url(${cocktail.strDrinkThumb})`"
+        class="pic"></div>
+      <div class="name">{{ cocktail.strDrink }}</div>
+    </div>
+  </RouterLink>
+</template>
+
+<style lang="scss" scoped>
+@import '../assets/styles/main.scss';
+.root {
+  width: calc((100% - 76px) / 3);
+  padding-right: 38px;
+  padding-bottom: 20px;
+  &:nth-child(3n + 3) {
+    padding-right: 0;
+  }
+}
+
+.name {
+  padding-top: 15px;
+  letter-spacing: 0.1em;
+}
+
+.pic {
+  width: 120px;
+  height: 120px;
+  margin: 0 auto;
+  background-repeat: no-repeat;
+  background-position: 50% 50%;
+  background-size: 100%;
+}
+</style>
