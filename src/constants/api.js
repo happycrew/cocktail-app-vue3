7b036afb31bda0ebe6d881a85719509205030eef
Vladimir Orlov <satancrew.true@gmail.com>
2023-09-30 19:00:53 +0300

    feat: add functional for button random cocktail

--- src/constants/api.js
+++ src/constants/api.js
@@ -2,3 +2,4 @@ const BASE_URL = 'https://www.thecocktaildb.com/api/json/v1/1/';
 export const INGREDIENTS_URL = `${BASE_URL}list.php?i=list`;
 export const COCKTAILS_BY_INGREDIENT = `${BASE_URL}filter.php?i=`;
 export const COCKTAIL_BY_ID = `${BASE_URL}lookup.php?i=`;
+export const COCKTAIL_BY_RANDOM = `${BASE_URL}random.php`;
